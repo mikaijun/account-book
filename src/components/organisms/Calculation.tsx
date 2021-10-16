76dc0ee7f5d81eb67839c259f7d2d6744bf1678b
tempe443 <mj.tempe443@gmail.com>
2021-10-16 19:07:19 +0900

    不要の記述の削除

--- src/components/organisms/Calculation.tsx
+++ src/components/organisms/Calculation.tsx
@@ -1,11 +1,7 @@
-import { useContext, useEffect } from "react";
 import { YearMonth } from "../molecules/YearMonth";
 import { Forms } from "../molecules/Forms";
-import { AccountBookContext } from "../../contexts/AccountBookContexts";
 
 export const Calculation = () => {
-  const { yearMonth, income, expense } = useContext(AccountBookContext);
-  useEffect(() => {}, [yearMonth, income, expense]);
   return (
     <div>
       <Forms />
